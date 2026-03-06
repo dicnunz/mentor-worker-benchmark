5e0bdc3572ff5516929e86f441a04673a30dadaa
nic <139033898+dicnunz@users.noreply.github.com>
2026-03-06 16:05:35 -0500

    benchmark: fix task pack validation import cycle

--- mentor_worker_benchmark/tasks/task_pack_v2/__init__.py
+++ mentor_worker_benchmark/tasks/task_pack_v2/__init__.py
@@ -1,6 +1,13 @@
 """Task pack v2 (mini-repo realism corpus)."""
 
 from mentor_worker_benchmark.tasks.task_pack_v2.pack import load_task_pack_v2, read_pack_metadata
-from mentor_worker_benchmark.tasks.task_pack_v2.provenance import write_provenance_artifacts
+
+
+def write_provenance_artifacts(*args, **kwargs):
+    from mentor_worker_benchmark.tasks.task_pack_v2.provenance import (
+        write_provenance_artifacts as _write_provenance_artifacts,
+    )
+
+    return _write_provenance_artifacts(*args, **kwargs)
 
 __all__ = ["load_task_pack_v2", "read_pack_metadata", "write_provenance_artifacts"]
